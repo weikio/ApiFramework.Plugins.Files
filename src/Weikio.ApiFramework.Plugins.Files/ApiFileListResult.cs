c50d33a264d2b204a3529830db3d1d502fa1ed33
Antti Kiiskinen <antti.kiiskinen@adafy.com>
2019-11-15 14:04:18 +0200

    File listing in directory

---
+++ src/Weikio.ApiFramework.Plugins.Files/ApiFileListResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Weikio.ApiFramework.Plugins.Files
+{
+    public partial class FilesApi
+    {
+        public class ApiFileListResult
+        {
+            public string Directory { get; set; }
+            public List<string> Files { get; set; }
+        }
+    }
+}
--- src/Weikio.ApiFramework.Plugins.Files/FilesApi.cs
+++ src/Weikio.ApiFramework.Plugins.Files/FilesApi.cs
@@ -1,13 +1,15 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.StaticFiles;
 
 namespace Weikio.ApiFramework.Plugins.Files
 {
-    public class FilesApi
+    public partial class FilesApi
     {
         public FileApiConfiguration Configuration { get; set; }
 
-        public async Task<ApiFileResult> GetFile(string filePath)
+        public async Task<ApiFileResult> GetFileAsync(string filePath)
         {
             var result = new ApiFileResult();
             var path = Path.Combine(Configuration.RootPath, filePath);
@@ -18,7 +20,32 @@ namespace Weikio.ApiFramework.Plugins.Files
                 await file.ReadAsync(result.Bytes, 0, (int)file.Length);
             }
 
+            var mimeConverter = new FileExtensionContentTypeProvider();
+            
+            if (mimeConverter.TryGetContentType(filePath, out var mimeType))
+            {
+                result.MimeType = mimeType;
+            }
+            
             return result;
+        }
+
+        public ApiFileListResult ListFiles(string directory = "")
+        {
+            string path;
+
+            if (string.IsNullOrWhiteSpace(directory))
+            {
+                path = Configuration.RootPath;
+            }
+            else
+            {
+                path = Path.Combine(Configuration.RootPath, directory);
+            }
+
+            var files = Directory.GetFiles(path);
+
+            return new ApiFileListResult { Directory = directory, Files = new List<string>(files) };
         }
     }
 }
